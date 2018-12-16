05093108a60549b6732a759904c6cb68edf72d4e
Юрий Владимирович <geroge.gru@gmail.com>
2018-12-16 13:58:25 +0200

    Semantic Ui setup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import "semantic-ui-css/semantic.min.css";
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
