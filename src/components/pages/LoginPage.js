d8e2036787335902415f82845f070cd269830be3
Юрий Владимирович <geroge.gru@gmail.com>
2018-12-25 15:16:21 +0200

    User reducer and user api request setup

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { login } from "../../actions/auth";
 
 import LoginForm from "../forms/LoginForm";
 
 class LoginPage extends Component {
-  submit = data => {
-    console.log(data);
-  };
+  submit = data =>
+    this.props.login(data).then(() => this.props.history.push("/"));
+
   render() {
     return (
       <div>
@@ -17,4 +20,14 @@ class LoginPage extends Component {
   }
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { login }
+)(LoginPage);
